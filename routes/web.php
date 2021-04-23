0a2766ed79dd4777d9750025af913fc4b9aaf35c
unknown <znickel.ions@gmail.com>
2021-04-23 20:20:24 +0100

    pushing recent changes to git

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use Illuminate\Support\Facades\File;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $files = File::files(resource_path("posts"));
+    $posts = [];
+
+    foreach($files as $file) {
+        $document = YamlFrontMatter::parseFile($file);
+
+        $posts[] += new Post( 
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body()
+        );
+    }
+
+    return view('posts', [
+        'posts' => $posts
+    ]);
+    // return view('posts', [
+    //     'posts' => Post::findAll()
+    // ]);
 });
+
+Route::get('/post/{post}', function($slug) {
+    $post = Post::find($slug);
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+');
